add27923ce557d266c7f33635fa03256a0410b54
chasomin <somin49@naver.com>
2024-02-14 21:31:03 +0900

    BaseViewController 생성

---
+++ ReminderApp/Base/BaseViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseViewController.swift
+//  ReminderApp
+//
+//  Created by 차소민 on 2/14/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+
+}
