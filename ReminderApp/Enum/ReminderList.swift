e7b71ada18d50652ce6476c958bdb2373b17b4d4
chasomin <somin49@naver.com>
2024-02-14 19:45:08 +0900

    main 뷰 컬렉션 뷰 항목들 enum으로 빼기 (타이틀, 이미지, 컬러)

--- ReminderApp/Enum/ReminderList.swift
+++ ReminderApp/Enum/ReminderList.swift
@@ -5,4 +5,42 @@
 //  Created by 차소민 on 2/14/24.
 //
 
-import Foundation
+import UIKit
+
+enum ReminderList: String, CaseIterable {
+    case today = "오늘"
+    case schedule = "예정"
+    case all = "전체"
+    case flag = "깃발 표시"
+    case done = "완료됨"
+    
+    var image: UIImage? {
+        switch self {
+        case .today:
+            UIImage(systemName: "calendar")
+        case .schedule:
+            UIImage(systemName: "calendar")
+        case .all:
+            UIImage(systemName: "tray.fill")
+        case .flag:
+            UIImage(systemName: "flag.fill")
+        case .done:
+            UIImage(systemName: "checkmark")
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .today:
+            UIColor.systemBlue
+        case .schedule:
+            UIColor.systemRed
+        case .all:
+            UIColor.lightGray
+        case .flag:
+            UIColor.systemYellow
+        case .done:
+            UIColor.systemGray
+        }
+    }
+}
