11a9e8ba33b08cb33af1fb9fe24374904d26bd79
chasomin <somin49@naver.com>
2024-02-14 19:47:20 +0900

    새로운 할 일 추가하는 뷰에서 tableView 타이틀 enum으로 빼기

---
+++ ReminderApp/Enum/AddReminderCellList.swift
@@ -0,0 +1,15 @@
+//
+//  AddReminderCellList.swift
+//  ReminderApp
+//
+//  Created by 차소민 on 2/14/24.
+//
+
+import Foundation
+
+enum AddReminderCellList: String, CaseIterable {
+    case deadline = "마감일"
+    case tag = "태그"
+    case priority = "우선 순위"
+    case image = "이미지 추가"
+}
