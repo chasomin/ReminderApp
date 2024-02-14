52f5e628f02ec18c4059171d9f7f591fb5e60742
chasomin <somin49@naver.com>
2024-02-14 17:34:28 +0900

    Circle image View custom 생성

--- ReminderApp/custom/CircleImageView.swift
+++ ReminderApp/custom/CircleImageView.swift
@@ -5,4 +5,27 @@
 //  Created by 차소민 on 2/14/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class CircleImageView: UIView {
+    let image = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(image)
+        image.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(7)
+        }
+        image.tintColor = .white
+        
+    }
+    override func layoutSubviews() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
