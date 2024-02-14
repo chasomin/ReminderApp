df2132e4b7a036a32073db3268ffea6580e31b02
chasomin <somin49@naver.com>
2024-02-14 19:46:24 +0900

    ViewController에서 자주 사용하는 것들 Extension 함수로 빼기(네비게이션 타이틀/바버튼, 툴바, 테이블/컬렉션뷰 delegate register설정)

--- ReminderApp/Extension/UIViewController+Extension.swift
+++ ReminderApp/Extension/UIViewController+Extension.swift
@@ -5,4 +5,38 @@
 //  Created by 차소민 on 2/14/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func setNavigationTitle(title: String, isLarge: Bool) {
+        navigationItem.title = title
+        navigationController?.navigationBar.prefersLargeTitles = isLarge
+        
+    }
+    func setToolbar(items: [UIBarButtonItem]) {
+        navigationController?.isToolbarHidden = false
+        toolbarItems = items
+        navigationController?.toolbar.barTintColor = .white
+    }
+    func setCollectionView(collectionView: UICollectionView, delegate: UICollectionViewDelegate, dataSource: UICollectionViewDataSource, cell: AnyClass, id: String) {
+        collectionView.delegate = delegate
+        collectionView.dataSource = dataSource
+        collectionView.register(cell, forCellWithReuseIdentifier: id)
+    }
+
+    func setTableView(tableView: UITableView, delegate: UITableViewDelegate, dataSource: UITableViewDataSource, cell: AnyClass, id: String) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+        tableView.register(cell, forCellReuseIdentifier: id)
+    }
+    
+    func setNavigationRightBarButton(title: String, action: Selector?) {
+        let barButton = UIBarButtonItem(title: title, style: .plain, target: self, action: action)
+        navigationItem.rightBarButtonItem = barButton
+    }
+    
+    func setNavigationLeftBarButton(title: String, action: Selector?) {
+        let barButton = UIBarButtonItem(title: title, style: .plain, target: self, action: action)
+        navigationItem.leftBarButtonItem = barButton
+    }
+}
