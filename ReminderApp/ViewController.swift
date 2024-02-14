4a2780c991b7c87d7a75d8abbbc5083d6485026f
chasomin <somin49@naver.com>
2024-02-14 14:13:57 +0900

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by 차소민 on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
