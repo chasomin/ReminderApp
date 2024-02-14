0efb1b2a3f8e123a5be24e729e1d2a131f7d9e05
chasomin <somin49@naver.com>
2024-02-14 21:34:00 +0900

    AddDetailTableViewCell 커스텀셀로 변경

--- ReminderApp/Add/AddDetailTableViewCell.swift
+++ ReminderApp/Add/AddDetailTableViewCell.swift
@@ -6,17 +6,43 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class AddDetailTableViewCell: BaseTableViewCell {
+    let title = UILabel()
+    let subTitle = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(title)
+        contentView.addSubview(subTitle)
+    }
+    
+    override func configureLayout() {
+        title.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(10)
+            make.leading.equalToSuperview().inset(15)
+            make.width.equalTo(80)
+            
+        }
+        subTitle.snp.makeConstraints { make in
+            make.leading.equalTo(title.snp.trailing).offset(5)
+            make.verticalEdges.equalToSuperview().inset(10)
+            make.trailing.equalToSuperview().inset(15)
+        }
+    }
 
     override func configureView() {
         backgroundColor = .systemGray5
         accessoryType = .disclosureIndicator
         selectionStyle = .none
-        detailTextLabel?.textAlignment = .right
+        
+        subTitle.font = .systemFont(ofSize: 13)
+        subTitle.textColor = .lightGray
     }
     
-    func configureCell(index: Int) {
-        textLabel?.text = AddReminderCellList.allCases[index-1].rawValue
+    func configureCell(index: Int, data: [String:String]) {
+        title.text = AddReminderCellList.allCases[index-1].rawValue
+        subTitle.text = data[AddReminderCellList.allCases[index-1].rawValue]
     }
 }
