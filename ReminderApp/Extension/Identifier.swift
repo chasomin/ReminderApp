3b31d1d9a1af298e2b71cb0847a30ec6af874aa4
chasomin <somin49@naver.com>
2024-02-14 19:43:48 +0900

    tableviewcell, collectionviewcell id extension으로 빼기

--- ReminderApp/Extension/Identifier.swift
+++ ReminderApp/Extension/Identifier.swift
@@ -5,4 +5,16 @@
 //  Created by 차소민 on 2/14/24.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewCell {
+    static var id: String {
+        return self.description()
+    }
+}
+
+extension UICollectionViewCell {
+    static var id: String {
+        return self.description()
+    }
+}
