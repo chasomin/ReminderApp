fdbd7f3ad82c1490d204ecf185f802694d829c0b
chasomin <somin49@naver.com>
2024-02-15 21:33:49 +0900

    Realm 모델 추가

---
+++ ReminderApp/Model/ReminderModel.swift
@@ -0,0 +1,29 @@
+//
+//  ReminderModel.swift
+//  ReminderApp
+//
+//  Created by 차소민 on 2/15/24.
+//
+
+import Foundation
+import RealmSwift
+
+class ReminderModel: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var memo: String?
+    @Persisted var deadline: String
+    @Persisted var tag: String?
+    @Persisted var priority: Int
+//    @Persisted var image
+    
+    
+    convenience init(title: String, memo: String? = nil, deadline: String, tag: String, priority: Int) {
+        self.init()
+        self.title = title
+        self.memo = memo
+        self.deadline = deadline
+        self.tag = tag
+        self.priority = priority
+    }
+}
