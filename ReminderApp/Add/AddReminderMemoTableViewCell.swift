d336b68e7a536b1c71f8a490a5c359f4515dbd0b
chasomin <somin49@naver.com>
2024-02-15 21:36:46 +0900

    추가 뷰에서 메모 입력 textView 생성

---
+++ ReminderApp/Add/AddReminderMemoTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  AddReminderMemoTableViewCell.swift
+//  ReminderApp
+//
+//  Created by 차소민 on 2/15/24.
+//
+
+import UIKit
+import SnapKit
+
+class AddReminderMemoTableViewCell: BaseTableViewCell {
+
+    let textView = UITextView()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(textView)
+    }
+    
+    override func configureLayout() {
+        textView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(5)
+            
+        }
+    }
+    //TODO: 플레이스홀더
+    override func configureView() {
+        textView.textColor = .white
+        textView.backgroundColor = .clear
+        backgroundColor = .systemGray5
+        
+        self.selectionStyle = .none
+        
+        
+    }
+}
