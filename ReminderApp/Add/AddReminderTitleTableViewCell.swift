dc8cc4f112bd92aee6cdd2b82c73e93dc114eddf
chasomin <somin49@naver.com>
2024-02-15 21:37:35 +0900

    추가뷰 타이틀 입력 txtField 뷰 생성

---
+++ ReminderApp/Add/AddReminderTitleTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  AddReminderTableViewCell.swift
+//  ReminderApp
+//
+//  Created by 차소민 on 2/14/24.
+//
+
+import UIKit
+import SnapKit
+
+class AddReminderTitleTableViewCell: BaseTableViewCell {
+
+    let textField = UITextField()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(textField)
+    }
+    
+    override func configureLayout() {
+        textField.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(5)
+            
+        }
+    }
+    
+    override func configureView() {
+        textField.placeholder = "   제목"
+        textField.textColor = .white
+        
+        backgroundColor = .systemGray5
+        
+        self.selectionStyle = .none
+        
+    }
+}
