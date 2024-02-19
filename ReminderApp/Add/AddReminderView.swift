cdd5cc1d4d5e52fe4ef8352f1e410feb295c6ab8
chasomin <somin49@naver.com>
2024-02-19 23:44:11 +0900

    Add View 수정

--- ReminderApp/Add/AddReminderView.swift
+++ ReminderApp/Add/AddReminderView.swift
@@ -12,34 +12,42 @@ final class AddReminderView: BaseView {
     
     let tableView = UITableView(frame: .zero, style: .insetGrouped)
     let deleteButton = UIButton()
-
+    let stackView = UIStackView()
     
     override func configureHierarchy() {
-        addSubview(tableView)
-        addSubview(deleteButton)
+        addSubview(stackView)
+        stackView.addArrangedSubview(tableView)
+        stackView.addArrangedSubview(deleteButton)
     }
     
     override func configureLayout() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+        
         tableView.snp.makeConstraints { make in
             make.top.horizontalEdges.equalToSuperview()
         }
+        
         deleteButton.snp.makeConstraints { make in
-            make.top.equalTo(tableView.snp.bottom)
             make.horizontalEdges.equalToSuperview().inset(20)
-            make.bottom.equalTo(safeAreaLayoutGuide).inset(40)
+            make.bottom.equalToSuperview().inset(40)
             make.height.equalTo(40)
         }
     }
     
     override func configureView() {
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        
         tableView.backgroundColor = .black
         tableView.separatorStyle = .singleLine
         
-
         deleteButton.setTitle("삭제", for: .normal)
         deleteButton.layer.cornerRadius = 10
         deleteButton.layer.borderColor = UIColor.white.cgColor
         deleteButton.layer.borderWidth = 1
-
     }
 }
