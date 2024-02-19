3732019aad75b42a7d4ac32ad7a6d823863f37d9
chasomin <somin49@naver.com>
2024-02-19 19:08:18 +0900

    이미지 추가 뷰에서 사용하는 enum 수정, 사용할 연산 프로퍼티, 메서드 추가 구현

--- ReminderApp/Enum/AddReminderCellList.swift
+++ ReminderApp/Enum/AddReminderCellList.swift
@@ -7,9 +7,59 @@
 
 import Foundation
 
-enum AddReminderCellList: String, CaseIterable {
-    case deadline = "마감일"
-    case tag = "태그"
-    case priority = "우선 순위"
-//    case image = "이미지 추가"
+enum AddReminderCellList: Int, CaseIterable {
+    case deadline = 1
+    case tag
+    case priority
+    case image
+    
+    var title: String {
+        switch self {
+        case .deadline:
+            "마감일"
+        case .tag:
+            "태그"
+        case .priority:
+            "우선 순위"
+        case .image:
+            "이미지 추가"
+        }
+    }
+    
+    func setPriority(data: ReminderModel) -> String {
+        switch data.priority {
+        case 0 :
+            return "없음"
+        case 1:
+            return "낮음"
+        case 2:
+            return "중간"
+        case 3:
+            return "높음"
+        default:
+            return ""
+        }
+    }
+    
+    func setSubTitle(data: ReminderModel) -> String{
+        switch self {
+        case .deadline:
+            data.deadline.dateToString()
+        case .tag:
+            data.tag ?? ""
+        case .priority:
+            setPriority(data: data)
+        case .image:
+            ""
+        }
+    }
+    
+    func setCellSize() -> CGFloat {
+        switch self {
+        case .image:
+            100
+        default:
+            50
+        }
+    }
 }
